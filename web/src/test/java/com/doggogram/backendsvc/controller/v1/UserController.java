97aac5afec054b56207fc22f62828aba6ced9cb1
D073980 <robin.herder@sap.com>
2019-11-11 16:49:22 +0100

    Added API Mappings

--- web/src/test/java/com/doggogram/backendsvc/controller/v1/UserController.java
+++ web/src/test/java/com/doggogram/backendsvc/controller/v1/UserController.java
@@ -1,7 +1,9 @@
 package com.doggogram.backendsvc.controller.v1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping (value = "/api/v1/user")
 public class UserController {
 }
