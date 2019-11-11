15941eb3df88dd2e1d9c6e46019fc3dbeaf4db44
D073980 <robin.herder@sap.com>
2019-11-11 15:45:27 +0100

    Created

---
+++ web/src/main/java/com/doggogram/backendsvc/BackendSvcApplication.java
@@ -0,0 +1,12 @@
+package com.doggogram.backendsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class BackendSvcApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(BackendSvcApplication.class, args);
+    }
+
+}
