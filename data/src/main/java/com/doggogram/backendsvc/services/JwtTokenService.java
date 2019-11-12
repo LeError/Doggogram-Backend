a09a191e18176ed933eb0c71cc804b76d0ce06f4
D073980 <robin.herder@sap.com>
2019-11-12 13:36:01 +0100

    Added JwtTokenService for Token creation and working with tokens

--- data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
@@ -1,8 +1,15 @@
 package com.doggogram.backendsvc.services;
 
+import io.jsonwebtoken.Claims;
+
 import java.util.Date;
+import java.util.Optional;
+import java.util.function.Function;
 
 public interface JwtTokenService {
     String generateToken(String user);
-    Date calculateExpirationDate(Date createdDate);
+    String getUserFromToken(String token);
+    Date getExpirationDateFromToken(String token);
+    <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver);
+    Optional<Boolean> validateToken(String token);
 }
--- data/src/main/java/com/doggogram/backendsvc/services/impl/JwtTokenServiceImpl.java
+++ data/src/main/java/com/doggogram/backendsvc/services/impl/JwtTokenServiceImpl.java
@@ -1,6 +1,7 @@
 package com.doggogram.backendsvc.services.impl;
 
 import com.doggogram.backendsvc.services.JwtTokenService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 public class JwtTokenServiceImpl implements JwtTokenService {
@@ -34,4 +37,34 @@ public class JwtTokenServiceImpl implements JwtTokenService {
     private Date calculateExpirationDate(Date createdDate) {
         return new Date(createdDate.getTime() + expiration * 10000);
     }
+
+    public String getUserFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+        .setSigningKey(secret)
+        .parseClaimsJws(token)
+        .getBody();
+    }
+
+    private Boolean isTokenNotExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.after(new Date());
+    }
+
+    public Optional<Boolean> validateToken(String token) {
+        return  isTokenNotExpired(token) ? Optional.of(Boolean.TRUE) : Optional.empty();
+    }
+
 }
