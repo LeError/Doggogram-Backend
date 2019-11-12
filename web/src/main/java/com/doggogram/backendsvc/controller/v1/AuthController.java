6588a54a7aca9fcd2a781d633c9192b802dc1f5d
D073980 <robin.herder@sap.com>
2019-11-12 13:52:49 +0100

    Controller for Authentication mapping, defined request type and exception handling

---
+++ web/src/main/java/com/doggogram/backendsvc/controller/v1/AuthController.java
@@ -0,0 +1,36 @@
+package com.doggogram.backendsvc.controller.v1;
+
+import com.doggogram.backendsvc.security.AuthRequest;
+import com.doggogram.backendsvc.services.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.persistence.EntityNotFoundException;
+
+@Controller
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @RequestMapping ( value = "/api/v1/auth/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createCustomer(@RequestBody AuthRequest authRequest) {
+        System.err.println(authRequest.getUser() + " " + authRequest.getPass());
+        return new ResponseEntity<>(authService.generateJWTToken(authRequest.getUser(), authRequest.getPass()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler (EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
