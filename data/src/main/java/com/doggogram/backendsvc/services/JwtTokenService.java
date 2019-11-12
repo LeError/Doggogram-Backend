201bb9d48bebdae41d82f33fcea841f0fb73f2c3
D073980 <robin.herder@sap.com>
2019-11-12 09:49:15 +0100

    Added Method to interface

--- data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
@@ -1,5 +1,8 @@
 package com.doggogram.backendsvc.services;
 
+import java.util.Date;
+
 public interface JwtTokenService {
     String generateToken(String user);
+    Date calculateExpirationDate(Date createdDate);
 }
