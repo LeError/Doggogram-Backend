6a0025db55b741f2c7f12203ede6920ca51e6391
D073980 <robin.herder@sap.com>
2019-11-12 13:57:01 +0100

    Added handle class for unauthed

---
+++ data/src/main/java/com/doggogram/backendsvc/security/JwtAuth.java
@@ -0,0 +1,50 @@
+package com.doggogram.backendsvc.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtAuth implements Authentication {
+
+    private final String token;
+
+    public JwtAuth(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
