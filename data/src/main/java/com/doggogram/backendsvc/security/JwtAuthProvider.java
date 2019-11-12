c3f7d70916c2fff56afb95e5f6b7c2c76e1fc495
D073980 <robin.herder@sap.com>
2019-11-12 14:04:47 +0100

    Added Auth Provider to validate auth

---
+++ data/src/main/java/com/doggogram/backendsvc/security/JwtAuthProvider.java
@@ -0,0 +1,47 @@
+package com.doggogram.backendsvc.security;
+
+import com.doggogram.backendsvc.services.JwtTokenService;
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthProvider implements AuthenticationProvider {
+
+    private final JwtTokenService jwtTokenService;
+
+    @SuppressWarnings("unused")
+    public JwtAuthProvider() {
+        this(null);
+    }
+
+    @Autowired
+    public JwtAuthProvider(JwtTokenService jwtService) {
+        this.jwtTokenService = jwtService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        try {
+            String token = (String) authentication.getCredentials();
+            String username = jwtTokenService.getUserFromToken(token);
+
+            return jwtTokenService.validateToken(token)
+            .map(aBoolean -> new JwtAuthedProfile(username))
+            .orElseThrow(() -> new JwtAuthException("JWT Token validation failed"));
+
+        } catch (JwtException ex) {
+            throw new JwtAuthException("Failed to verify token");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtAuth.class.equals(authentication);
+    }
+
+}
