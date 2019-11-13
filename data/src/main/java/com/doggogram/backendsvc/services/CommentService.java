b1d0125212eea9e027480738b49f5d83152286f8
D073980 <robin.herder@sap.com>
2019-11-13 21:27:36 +0100

    Created Service Interfaces

---
+++ data/src/main/java/com/doggogram/backendsvc/services/CommentService.java
@@ -0,0 +1,11 @@
+package com.doggogram.backendsvc.services;
+
+import com.doggogram.backendsvc.dto.CommentDTO;
+
+import java.util.List;
+
+public interface CommentService extends RestService<List<CommentDTO>> {
+
+
+
+}
---
+++ data/src/main/java/com/doggogram/backendsvc/services/ImageService.java
@@ -0,0 +1,11 @@
+package com.doggogram.backendsvc.services;
+
+import com.doggogram.backendsvc.dto.ImageDTO;
+
+import java.util.List;
+
+public interface ImageService extends RestService<List<ImageDTO>> {
+
+
+
+}
