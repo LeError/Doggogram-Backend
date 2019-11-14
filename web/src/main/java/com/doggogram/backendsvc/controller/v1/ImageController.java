01801347a252db1a2371f8f1eee568c0d13a6be3
D073980 <robin.herder@sap.com>
2019-11-14 12:50:09 +0100

    Added v1 of ImageController

--- web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
+++ web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
@@ -1,9 +1,69 @@
 package com.doggogram.backendsvc.controller.v1;
 
+import com.doggogram.backendsvc.services.ImageService;
+import com.doggogram.backendsvc.services.StorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
-@RequestMapping (value = "/api/v1/image")
+@RequestMapping("/api/v1/storage")
 public class ImageController {
+
+    private final StorageService storageService;
+    private final ImageService imageService;
+
+    public ImageController (StorageService storageService, ImageService imageService) {
+        this.storageService = storageService;
+        this.imageService = imageService;
+    }
+
+    @GetMapping ("/files/filenames/all")
+    public String listUploadedFiles() throws IOException {
+        return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity serveFile(@PathVariable String filename) {
+        try {
+            Resource file = storageService.loadAsResource(filename);
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping({"/file/upload", "/file/upload/"})
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("user") String user, @RequestParam("bio") String bio, @RequestParam("title") String title) {
+        try {
+            imageService.addImage(user, title, bio);
+            storageService.store(file);
+            return new ResponseEntity<>("File Upload Accepted!", HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("", HttpStatus.CONFLICT);
+        }
+    }
+
+    /*
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+
+     */
 }
