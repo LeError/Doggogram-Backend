638b49625851f007ed0ab710890eb51108db662c
D073980 <robin.herder@sap.com>
2019-11-14 12:47:35 +0100

    Added Storage file not found exception

---
+++ data/src/main/java/com/doggogram/backendsvc/storage/exceptions/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.storage.exceptions;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
