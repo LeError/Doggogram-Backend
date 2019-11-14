3d245011480692f5ec30b766c7c48dd8a30f4e8d
D073980 <robin.herder@sap.com>
2019-11-14 12:50:32 +0100

    Added second constructor

--- data/src/main/java/com/doggogram/backendsvc/security/exceptions/JwtAuthException.java
+++ data/src/main/java/com/doggogram/backendsvc/security/exceptions/JwtAuthException.java
@@ -3,7 +3,13 @@ package com.doggogram.backendsvc.security.exceptions;
 import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthException extends AuthenticationException {
-    public JwtAuthException(String msg) {
-        super(msg);
+
+    public JwtAuthException(String message) {
+        super(message);
     }
+
+    public JwtAuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
