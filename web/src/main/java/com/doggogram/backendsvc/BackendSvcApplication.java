9800af6c7a7ccce261626247e533267192f17a93
D073980 <robin.herder@sap.com>
2019-11-14 12:43:05 +0100

    Reformatted

--- web/src/main/java/com/doggogram/backendsvc/BackendSvcApplication.java
+++ web/src/main/java/com/doggogram/backendsvc/BackendSvcApplication.java
@@ -3,7 +3,8 @@ package com.doggogram.backendsvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class BackendSvcApplication {
+@SpringBootApplication
+public class BackendSvcApplication {
 
     public static void main (String[] args) {
         SpringApplication.run(BackendSvcApplication.class, args);
