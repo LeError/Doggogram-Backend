5441d9ad89a68149fe1c10484206eb52359e0320
D073980 <robin.herder@sap.com>
2019-11-15 15:07:58 +0100

    changed url for Linux (Debian 10)

--- data/src/main/java/com/doggogram/backendsvc/storage/properties/StorageProperties.java
+++ data/src/main/java/com/doggogram/backendsvc/storage/properties/StorageProperties.java
@@ -3,11 +3,13 @@ package com.doggogram.backendsvc.storage.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+
 @ConfigurationProperties ("storage")
 @Component
 public class StorageProperties {
 
-    private String location = "C:\\Users\\d073980\\IdeaProjects\\backend-svc\\data\\src\\main\\java\\com\\doggogram\\backendsvc\\storage\\storage";
+    private String location = new File("/var/doggogramsvc/imgs").getAbsolutePath();
 
     public String getLocation() {
         return location;
