75a0757be6d2d9a5b11253be8175196a41f849f6
D073980 <robin.herder@sap.com>
2019-11-20 09:42:14 +0100

    Moved methods and changed implementation for base64 encoded Images

--- data/src/main/java/com/doggogram/backendsvc/services/ImageService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/ImageService.java
@@ -1,14 +1,13 @@
 package com.doggogram.backendsvc.services;
 
 import com.doggogram.backendsvc.dto.ImageDTO;
-import com.doggogram.backendsvc.dto.ImageFilenameDTO;
+import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
 
 import java.util.List;
 
 public interface ImageService extends RestService<List<ImageDTO>> {
 
     String addImage(String user, String title, String bio, String filename);
-    List<ImageFilenameDTO> getAllImageFilenames();
-    String getImageName(String user, String fileExtension);
+    ImageDTO getItemById(long id) throws EntityCorruptedException;
 
 }
--- data/src/main/java/com/doggogram/backendsvc/services/impl/ImageServiceImpl.java
+++ data/src/main/java/com/doggogram/backendsvc/services/impl/ImageServiceImpl.java
@@ -3,31 +3,36 @@ package com.doggogram.backendsvc.services.impl;
 import com.doggogram.backendsvc.domain.Image;
 import com.doggogram.backendsvc.domain.User;
 import com.doggogram.backendsvc.dto.ImageDTO;
-import com.doggogram.backendsvc.dto.ImageFilenameDTO;
 import com.doggogram.backendsvc.mapper.ImageMapper;
 import com.doggogram.backendsvc.repositories.ImageRepository;
 import com.doggogram.backendsvc.repositories.UserRepository;
 import com.doggogram.backendsvc.services.ImageService;
+import com.doggogram.backendsvc.services.StorageService;
+import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
     private ImageRepository imageRepository;
     private ImageMapper imageMapper;
     private UserRepository userRepository;
+    private StorageService storageService;
 
-    public ImageServiceImpl(ImageRepository imageRepository, ImageMapper imageMapper, UserRepository userRepository) {
+    public ImageServiceImpl(ImageRepository imageRepository, ImageMapper imageMapper, UserRepository userRepository, StorageService storageService) {
         this.imageRepository = imageRepository;
         this.imageMapper = imageMapper;
         this.userRepository = userRepository;
+        this.storageService = storageService;
     }
 
     @Override
@@ -51,21 +56,38 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public List<ImageDTO> getAllItems () {
-        return imageRepository.findAll().stream().map(imageMapper::imageToImageDTO).collect(Collectors.toList());
+        List<ImageDTO> images = imageRepository.findAll().stream().map(imageMapper::imageToImageDTO).collect(Collectors.toList());
+        List<ImageDTO> corruptedImages = new ArrayList<>();
+        for(ImageDTO image : images) {
+            try {
+                image.setImageEncoded(storageService.getEncodedImage(image.getFilename()));
+            } catch (IOException e) {
+                log.error("Could not load image for " + image.getId() + ", so it will be removed!");
+                imageRepository.deleteImageById(image.getId());
+                log.info("Removed Image " + image.getId() + " from Database!");
+                corruptedImages.add(image);
+            }
+        }
+        images.removeAll(corruptedImages);
+        log.info("Removed " + corruptedImages.size() + "Corrupted Images from Database!");
+        return images;
     }
 
     @Override
-    public List<ImageFilenameDTO> getAllImageFilenames() {
-        return imageRepository.findAll().stream().map(imageMapper::imageToImageFilenameDTO).collect(Collectors.toList());
+    public ImageDTO getItemById (long id) throws EntityNotFoundException, EntityCorruptedException {
+        ImageDTO imageDTO = imageMapper.imageToImageDTO(imageRepository.findById(id));
+        if(imageDTO == null) {
+            throw new EntityNotFoundException();
+        }
+        try {
+            imageDTO.setImageEncoded(storageService.getEncodedImage(imageDTO.getFilename()));
+        } catch (IOException e) {
+            log.error("Could not load image for " + imageDTO.getId() + ", so it will be removed!");
+            imageRepository.deleteImageById(imageDTO.getId());
+            log.info("Removed Image " + imageDTO.getId() + " from Database!");
+            throw new EntityCorruptedException("Can't return corrupted entity!");
+        }
+        return imageDTO;
     }
 
-    @Override
-    public String getImageName(String user, String fileExtension) {
-        String filename = user;
-        Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("--dd-MM-yyyy--HH-mm-ss");
-        filename += dateFormat.format(date);
-        filename += "." + fileExtension;
-        return filename;
-    }
 }
