0c402db260d5110b77c932c2cb238d03fd516b38
D073980 <robin.herder@sap.com>
2019-11-20 09:55:00 +0100

    Rearranged methods and added new mappings

--- web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
+++ web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
@@ -1,10 +1,13 @@
 package com.doggogram.backendsvc.controller.v1;
 
-import com.doggogram.backendsvc.dto.ImageFilenameListDTO;
+import com.doggogram.backendsvc.dto.ImageListDTO;
 import com.doggogram.backendsvc.services.ImageService;
 import com.doggogram.backendsvc.services.StorageService;
 import com.doggogram.backendsvc.storage.exceptions.StorageFileNotFoundException;
+import com.doggogram.backendsvc.util.Util;
+import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
 import com.google.common.io.ByteStreams;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Controller
 @RequestMapping("/api/v1/storage")
 public class ImageController {
@@ -38,16 +42,11 @@ public class ImageController {
         return new ResponseEntity<>(imageService.count(), HttpStatus.OK);
     }
 
-    @GetMapping ({"/images/filenames/all", "/images/filenames/all/"})
-    public ResponseEntity listUploadedFiles() {
-        return new ResponseEntity<>(new ImageFilenameListDTO(imageService.getAllImageFilenames()) , HttpStatus.OK);
-    }
-
     @PostMapping({"/images/upload", "/images/upload/"})
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("user") String user, @RequestParam("bio") String bio, @RequestParam("title") String title) {
         try {
-            imageService.addImage(user, title, bio, imageService.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
-            storageService.store(file, imageService.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
+            imageService.addImage(user, title, bio, Util.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
+            storageService.store(file, Util.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
             return new ResponseEntity<>("File Upload Accepted!", HttpStatus.ACCEPTED);
         } catch (Exception e) {
             e.printStackTrace();
@@ -55,15 +54,30 @@ public class ImageController {
         }
     }
 
-    @GetMapping({"/images/load/{filename:.+}", "/images/load/{filename:.+}/"})
+    @GetMapping ({"/images/all", "/images/all/"})
+    public ResponseEntity listUploadedFiles() {
+        return new ResponseEntity<>(new ImageListDTO(imageService.getAllItems()) , HttpStatus.OK);
+    }
+
+    @GetMapping({"/images/file/{filename:.+}", "/images/file/{filename:.+}/"})
     @ResponseBody
     public ResponseEntity serveFile(@PathVariable String filename) {
         try {
             Resource file = storageService.loadAsResource(filename);
             return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(Base64Utils.encodeToString(ByteStreams.toByteArray(file.getInputStream())));
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
             return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping({"/images/image/{imageId}", "/images/image/{imageId}/"})
+    public ResponseEntity getImage(@PathVariable long imageId) {
+        try {
+            return new ResponseEntity(imageService.getItemById(imageId), HttpStatus.OK);
+        } catch (EntityCorruptedException e) {
+            log.error(e.getMessage());
+            return new ResponseEntity(e.getMessage(), HttpStatus.GONE);
         }
     }
 
