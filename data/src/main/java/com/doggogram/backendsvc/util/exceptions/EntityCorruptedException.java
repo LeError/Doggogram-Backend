373fbf88155a7ff58d3dbbe063b52a8d94571edd
D073980 <robin.herder@sap.com>
2019-11-20 09:43:01 +0100

    Added new Exception

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/EntityCorruptedException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class EntityCorruptedException extends Exception {
+
+    public EntityCorruptedException() {
+        super();
+    }
+
+    public EntityCorruptedException(String message) {
+        super(message);
+    }
+
+}
