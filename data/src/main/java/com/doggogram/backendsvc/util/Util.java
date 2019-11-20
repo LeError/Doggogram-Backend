95c14c5f5b8f7de43112658279e4eeb5465d93a8
D073980 <robin.herder@sap.com>
2019-11-20 09:43:47 +0100

    Added Utility class

---
+++ data/src/main/java/com/doggogram/backendsvc/util/Util.java
@@ -0,0 +1,19 @@
+package com.doggogram.backendsvc.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Util {
+
+    public static String getImageName(String user, String fileExtension) {
+        String filename = user;
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("--dd-MM-yyyy--HH-mm-ss");
+        filename += dateFormat.format(date);
+        filename += "." + fileExtension;
+        return filename;
+    }
+
+}
