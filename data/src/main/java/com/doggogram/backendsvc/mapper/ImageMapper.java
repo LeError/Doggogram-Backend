d9fab61b807099eb7b9adefff27bae9be56e8d69
D073980 <robin.herder@sap.com>
2019-11-20 09:27:00 +0100

    DEPRECATED

--- data/src/main/java/com/doggogram/backendsvc/mapper/ImageMapper.java
+++ data/src/main/java/com/doggogram/backendsvc/mapper/ImageMapper.java
@@ -2,7 +2,6 @@ package com.doggogram.backendsvc.mapper;
 
 import com.doggogram.backendsvc.domain.Image;
 import com.doggogram.backendsvc.dto.ImageDTO;
-import com.doggogram.backendsvc.dto.ImageFilenameDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -11,6 +10,5 @@ public interface ImageMapper {
 
     ImageMapper INSTANCE = Mappers.getMapper(ImageMapper.class);
     ImageDTO imageToImageDTO(Image image);
-    ImageFilenameDTO imageToImageFilenameDTO(Image image);
 
 }
