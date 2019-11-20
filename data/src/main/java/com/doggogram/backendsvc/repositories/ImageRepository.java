102b549756a053df0261a80a799c0050579e8b5d
D073980 <robin.herder@sap.com>
2019-11-20 09:42:37 +0100

    Added findById

--- data/src/main/java/com/doggogram/backendsvc/repositories/ImageRepository.java
+++ data/src/main/java/com/doggogram/backendsvc/repositories/ImageRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    Image findById(long id);
     void deleteImageById(Long id);
 }
