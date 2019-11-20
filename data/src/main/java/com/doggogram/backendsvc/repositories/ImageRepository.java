860262c7de2fd812587f7b7724a7ff45326642ab
D073980 <robin.herder@sap.com>
2019-11-20 09:24:19 +0100

    Added deleteImageById to Repository

--- data/src/main/java/com/doggogram/backendsvc/repositories/ImageRepository.java
+++ data/src/main/java/com/doggogram/backendsvc/repositories/ImageRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {}
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    void deleteImageById(Long id);
+}
