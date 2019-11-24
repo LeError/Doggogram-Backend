4683cc91f1b78aa40aa6b15f8b22cbce947897b7
D073980 <robin.herder@sap.com>
2019-11-24 17:30:27 +0100

    Made Transactional

--- data/src/main/java/com/doggogram/backendsvc/services/AuthService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/AuthService.java
@@ -2,6 +2,9 @@ package com.doggogram.backendsvc.services;
 
 import com.doggogram.backendsvc.security.responses.JwtTokenResponse;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface AuthService {
     JwtTokenResponse generateJWTToken (String user, String password);
 }
