12de8e161eeaf49609b6acf724566512662c635a
D073980 <robin.herder@sap.com>
2019-11-24 17:30:02 +0100

    Made Transactional

--- data/src/main/java/com/doggogram/backendsvc/services/ImageService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/ImageService.java
@@ -2,9 +2,11 @@ package com.doggogram.backendsvc.services;
 
 import com.doggogram.backendsvc.dto.ImageDTO;
 import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface ImageService extends RestService<List<ImageDTO>> {
 
     String addImage(String user, String title, String bio, String filename);
