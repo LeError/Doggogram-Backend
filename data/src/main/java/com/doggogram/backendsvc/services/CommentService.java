bec245cee2a02f8b866e7ab49f7b4e547f7c3068
D073980 <robin.herder@sap.com>
2019-11-24 17:30:11 +0100

    Made Transactional

--- data/src/main/java/com/doggogram/backendsvc/services/CommentService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/CommentService.java
@@ -2,8 +2,10 @@ package com.doggogram.backendsvc.services;
 
 import com.doggogram.backendsvc.dto.CommentDTO;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 public interface CommentService extends RestService<List<CommentDTO>> {
 
 
