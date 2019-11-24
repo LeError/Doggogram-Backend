4fce7e0c87ece513ec536b9e2bee16e09798922e
D073980 <robin.herder@sap.com>
2019-11-24 17:54:11 +0100

    Altered Attributes

--- data/src/main/java/com/doggogram/backendsvc/dto/CommentDTO.java
+++ data/src/main/java/com/doggogram/backendsvc/dto/CommentDTO.java
@@ -1,15 +1,16 @@
 package com.doggogram.backendsvc.dto;
 
-import com.doggogram.backendsvc.domain.Image;
 import com.doggogram.backendsvc.domain.User;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class CommentDTO {
 
     private Long id;
     private User user;
-    private Image image;
     private String comment;
+    private Date created;
 
 }
