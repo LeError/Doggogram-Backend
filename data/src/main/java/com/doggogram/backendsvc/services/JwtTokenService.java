15c823ab457406cb08827b25de25d66802828904
D073980 <robin.herder@sap.com>
2019-11-24 17:32:34 +0100

    Made Transactional

--- data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/JwtTokenService.java
@@ -2,10 +2,12 @@ package com.doggogram.backendsvc.services;
 
 import io.jsonwebtoken.Claims;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.Optional;
 import java.util.function.Function;
 
+@Transactional
 public interface JwtTokenService {
     String generateToken(String user);
     String getUserFromToken(String token);
