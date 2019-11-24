1e861cb08c35a2560dd5c21441ffaa44be2ba214
D073980 <robin.herder@sap.com>
2019-11-24 17:27:50 +0100

    Made Transactional and changed / added exceptions

--- data/src/main/java/com/doggogram/backendsvc/services/StorageService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/StorageService.java
@@ -1,22 +1,27 @@
 package com.doggogram.backendsvc.services;
 
+import com.doggogram.backendsvc.util.exceptions.ImageCorruptedException;
+import com.doggogram.backendsvc.util.exceptions.ImageNotFoundException;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.transaction.Transactional;
 import java.nio.file.Path;
 
+@Transactional
 public interface StorageService {
 
     void init();
 
     void store(MultipartFile file, String filename);
 
-    Path load(String filename);
+    Path load(String filename)
+    throws ImageNotFoundException;
 
-    Resource loadAsResource(String filename);
+    Resource loadAsResource(String filename)
+    throws ImageNotFoundException;
 
-    String getEncodedImage (String filename) throws IOException;
+    String getEncodedImage (String filename) throws ImageNotFoundException, ImageCorruptedException;
 
     void deleteAll();
 
