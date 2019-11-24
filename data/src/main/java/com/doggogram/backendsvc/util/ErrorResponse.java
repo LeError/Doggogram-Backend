7af662cc68231cbde31b5fb8f18dca81e5116376
D073980 <robin.herder@sap.com>
2019-11-24 18:28:45 +0100

    Added Attribute

--- data/src/main/java/com/doggogram/backendsvc/util/ErrorResponse.java
+++ data/src/main/java/com/doggogram/backendsvc/util/ErrorResponse.java
@@ -11,6 +11,7 @@ import java.util.List;
 @XmlRootElement (name = "error")
 public class ErrorResponse {
 
+    private String exception;
     private String message;
     private List<String> details;
 
