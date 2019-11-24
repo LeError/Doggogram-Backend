7c1e677c1f1a4127f2a3554bd31f11fe0e6afade
D073980 <robin.herder@sap.com>
2019-11-24 15:23:38 +0100

    Added Exceptions

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/ImageCorruptedException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class ImageCorruptedException extends Exception {
+
+    public ImageCorruptedException() {
+        super();
+    }
+
+    public ImageCorruptedException(String message) {
+        super(message);
+    }
+
+}
---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/ImageNotFoundException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class ImageNotFoundException extends Exception {
+
+    public ImageNotFoundException() {
+        super();
+    }
+
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+
+}
