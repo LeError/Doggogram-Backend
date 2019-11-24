8545b4f8182d20dc0aaf4a04357980b233bcca92
D073980 <robin.herder@sap.com>
2019-11-24 21:27:03 +0100

    Added new Exception Handling

--- web/src/main/java/com/doggogram/backendsvc/controller/v1/ErrorController.java
+++ web/src/main/java/com/doggogram/backendsvc/controller/v1/ErrorController.java
@@ -3,6 +3,7 @@ package com.doggogram.backendsvc.controller.v1;
 import com.doggogram.backendsvc.util.ErrorResponse;
 import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
 import com.doggogram.backendsvc.util.exceptions.ImageNotFoundException;
+import com.doggogram.backendsvc.util.exceptions.UserRegistrationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,6 +21,7 @@ public class ErrorController extends ResponseEntityExceptionHandler {
     private final String SERVER_ERROR = "Internal Server Error!";
     private final String NOT_FOUND = "Requested Resource not Found!";
     private final String GONE = "Requested Resource was corrupted!";
+    private final String NOT_ACCEPTABLE ="Registration not Possible!";
 
     @ExceptionHandler(EntityNotFoundException.class)
     public final ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e, WebRequest webRequest) {
@@ -45,11 +47,20 @@ public class ErrorController extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("ImageNotFoundException", NOT_FOUND, details), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(UserRegistrationException.class)
+    public final ResponseEntity<ErrorResponse> handleUserRegistrationException(ImageNotFoundException e, WebRequest webRequest) {
+        List<String> details = new ArrayList<>();
+        details.add(e.getMessage());
+        details.add(webRequest.getDescription(false));
+        return new ResponseEntity<>(new ErrorResponse("ImageNotFoundException", NOT_ACCEPTABLE, details), HttpStatus.NOT_ACCEPTABLE);
+    }
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorResponse> handleUnhandledExceptions(Exception e, WebRequest webRequest) {
         List<String> details = new ArrayList<>();
         System.err.println(e.getMessage());
         details.add(e.getMessage());
+        details.add(webRequest.getDescription(false));
         details.add("A unhandled Exception was thrown! Please Report to the Administrator / Support!");
         return new ResponseEntity<>(new ErrorResponse("Exception", SERVER_ERROR, details), HttpStatus.INTERNAL_SERVER_ERROR);
     }
