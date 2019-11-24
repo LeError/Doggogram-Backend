97d5799dde12d18a1cc68f5802ae5dc5ac38e82c
D073980 <robin.herder@sap.com>
2019-11-24 17:52:53 +0100

    Added Constructor and Attribute

--- data/src/main/java/com/doggogram/backendsvc/domain/Image.java
+++ data/src/main/java/com/doggogram/backendsvc/domain/Image.java
@@ -1,17 +1,33 @@
 package com.doggogram.backendsvc.domain;
 
+import com.doggogram.backendsvc.dto.ImageDTO;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.Date;
+import java.util.List;
 
 @Data
+@NoArgsConstructor
 @Entity (name = "IMAGE")
 public class Image {
+
+    public Image(ImageDTO imageDTO) {
+        id = imageDTO.getId();
+        filename = imageDTO.getFilename();
+        title = imageDTO.getTitle();
+        bio = imageDTO.getBio();
+        likes = imageDTO.getLikes();
+        created = imageDTO.getCreated();
+    }
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -32,5 +48,8 @@ public class Image {
 
     @Column (name = "IMAGE_CREATED")
     private Date created;
+
+    @OneToMany (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Comment> comments;
 
 }
