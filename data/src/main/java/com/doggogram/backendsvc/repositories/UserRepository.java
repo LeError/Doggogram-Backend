8321dbeb77529bd16c500b4a4f4aec7ff712bb46
D073980 <robin.herder@sap.com>
2019-11-24 17:34:47 +0100

    Added findUserByImageId

--- data/src/main/java/com/doggogram/backendsvc/repositories/UserRepository.java
+++ data/src/main/java/com/doggogram/backendsvc/repositories/UserRepository.java
@@ -2,11 +2,15 @@ package com.doggogram.backendsvc.repositories;
 
 import com.doggogram.backendsvc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     User findUserByUser(String user);
+
+    @Query (value = "SELECT USER_USER_NAME FROM USER_IMAGE WHERE images_IMAGE_ID = ?1", nativeQuery = true)
+    String findUserByImageId(Long id);
 
 }
