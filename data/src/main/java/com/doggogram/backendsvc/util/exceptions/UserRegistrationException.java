f946433a785db34da858a1f0b769cc2ab4101399
D073980 <robin.herder@sap.com>
2019-11-24 21:26:45 +0100

    Added Exception

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/UserRegistrationException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class UserRegistrationException extends Exception {
+
+    public UserRegistrationException() {
+        super();
+    }
+
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+
+}
