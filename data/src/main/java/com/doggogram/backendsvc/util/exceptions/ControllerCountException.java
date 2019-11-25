85275099aaedd6097bfbb0adf7e5a240484905db
D073980 <robin.herder@sap.com>
2019-11-25 12:49:07 +0100

    Added ControllerCountException

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/ControllerCountException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class ControllerCountException extends Exception {
+
+    public ControllerCountException() {
+        super();
+    }
+
+    public ControllerCountException(String message) {
+        super(message);
+    }
+
+}
