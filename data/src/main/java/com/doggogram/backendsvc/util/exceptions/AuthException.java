ef45941eeb016595d8b756ae121c8d17a69a02f4
D073980 <robin.herder@sap.com>
2019-11-25 12:20:01 +0100

    Added AuthException

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/AuthException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class AuthException extends Exception {
+
+    public AuthException() {
+        super();
+    }
+
+    public AuthException(String message) {
+        super(message);
+    }
+
+}
