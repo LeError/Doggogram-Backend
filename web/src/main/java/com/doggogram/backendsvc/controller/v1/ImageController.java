b80d8550af9dea601e33552a7ebdcfeaa47214b5
D073980 <robin.herder@sap.com>
2019-11-26 15:41:02 +0100

    Purged external storing of images

--- web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
+++ web/src/main/java/com/doggogram/backendsvc/controller/v1/ImageController.java
@@ -4,28 +4,20 @@ import com.doggogram.backendsvc.dto.ImageDTO;
 import com.doggogram.backendsvc.dto.ImageListDTO;
 import com.doggogram.backendsvc.services.ImageService;
 import com.doggogram.backendsvc.services.JwtTokenService;
-import com.doggogram.backendsvc.services.StorageService;
 import com.doggogram.backendsvc.util.Util;
 import com.doggogram.backendsvc.util.exceptions.ControllerCountException;
-import com.doggogram.backendsvc.util.exceptions.EntityCorruptedException;
-import com.doggogram.backendsvc.util.exceptions.ImageNotFoundException;
 import com.doggogram.backendsvc.util.exceptions.ImageUploadException;
-import com.google.common.io.ByteStreams;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
@@ -35,12 +27,10 @@ import javax.persistence.EntityNotFoundException;
 @RequestMapping("/api/v1/")
 public class ImageController {
 
-    private final StorageService storageService;
     private final ImageService imageService;
     private final JwtTokenService jwtTokenService;
 
-    public ImageController (StorageService storageService, ImageService imageService, JwtTokenService jwtTokenService) {
-        this.storageService = storageService;
+    public ImageController (ImageService imageService, JwtTokenService jwtTokenService) {
         this.imageService = imageService;
         this.jwtTokenService = jwtTokenService;
     }
@@ -65,8 +55,7 @@ public class ImageController {
                 case "gif": break;
                 default: throw new ImageUploadException("Not a allowed file extension! Only jpg, jpeg, png and gif are allowed!");
             }
-            imageService.addImage(user, title, bio, Util.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
-            storageService.store(file, Util.getImageName(user, FilenameUtils.getExtension(file.getOriginalFilename())));
+            imageService.addImage(user, file, title, bio);
             return new ResponseEntity<>("File Upload Accepted!", HttpStatus.ACCEPTED);
         } catch (Exception e) {
             throw new ImageUploadException(e.getMessage());
@@ -78,19 +67,9 @@ public class ImageController {
         return new ResponseEntity<>(new ImageListDTO(imageService.getAllItems()) , HttpStatus.OK);
     }
 
-    @GetMapping({"/images/file/{filename:.+}", "/images/file/{filename:.+}/"})
-    @ResponseBody
-    public ResponseEntity serveFile(@PathVariable String filename) throws ImageNotFoundException {
-        try {
-            Resource file = storageService.loadAsResource(filename);
-            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(Base64Utils.encodeToString(ByteStreams.toByteArray(file.getInputStream())));
-        } catch (Exception e) {
-            throw new ImageNotFoundException("Could not find Image with Filename " + filename + "!");
-        }
-    }
 
     @GetMapping({"/images/image/{imageId}", "/images/image/{imageId}/"})
-    public ResponseEntity<ImageDTO> getImage(@PathVariable long imageId) throws EntityNotFoundException, EntityCorruptedException {
+    public ResponseEntity<ImageDTO> getImage(@PathVariable long imageId) throws EntityNotFoundException {
         return new ResponseEntity(imageService.getItemById(imageId), HttpStatus.OK);
     }
 
