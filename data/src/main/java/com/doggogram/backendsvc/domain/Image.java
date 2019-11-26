b434f8f1bb6d94af4f85fd856d287ec4a60c196a
D073980 <robin.herder@sap.com>
2019-11-26 14:47:39 +0100

    Reworked Image Entity

--- data/src/main/java/com/doggogram/backendsvc/domain/Image.java
+++ data/src/main/java/com/doggogram/backendsvc/domain/Image.java
@@ -1,8 +1,6 @@
 package com.doggogram.backendsvc.domain;
 
-import com.doggogram.backendsvc.dto.ImageDTO;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,43 +9,35 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
 @Entity (name = "IMAGE")
 public class Image {
-
-    public Image(ImageDTO imageDTO) {
-        id = imageDTO.getId();
-        filename = imageDTO.getFilename();
-        title = imageDTO.getTitle();
-        bio = imageDTO.getBio();
-        likes = imageDTO.getLikes();
-        created = imageDTO.getCreated();
-    }
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (name = "IMAGE_ID")
     private Long id;
 
-    @Column (name = "IMAGE_FILE")
-    private String filename;
+    @Lob
+    @Column (name = "IMAGE_IMAGE", nullable = false)
+    private String image;
 
-    @Column (name = "IMAGE_TITLE")
+    @Column (name = "IMAGE_TITLE", nullable = false)
     private String title;
 
     @Column (name = "IMAGE_BIO")
     private String bio;
 
-    @Column (name = "IMAGE_LIKES")
-    private Long likes;
+    @Column (name = "IMAGE_LIKES", nullable = false)
+    private Long likes = 0L;
 
-    @Column (name = "IMAGE_CREATED")
-    private Date created;
+    @Column (name = "IMAGE_CREATED", nullable = false)
+    private Date created = new Date();
 
     @OneToMany (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Comment> comments;
