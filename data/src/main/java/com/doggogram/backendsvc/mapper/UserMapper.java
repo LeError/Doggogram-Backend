7efd2af6f992feec953ccb18eda4df9cc41e9f57
D073980 <robin.herder@sap.com>
2019-11-26 17:10:25 +0100

    Added Additional mapping

--- data/src/main/java/com/doggogram/backendsvc/mapper/UserMapper.java
+++ data/src/main/java/com/doggogram/backendsvc/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.doggogram.backendsvc.mapper;
 
 import com.doggogram.backendsvc.domain.User;
 import com.doggogram.backendsvc.dto.UserDTO;
+import com.doggogram.backendsvc.dto.UserImagesDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -10,5 +11,6 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
     UserDTO userToUserDTO(User user);
+    UserImagesDTO userToUserImagesDTO(User user);
 
 }
