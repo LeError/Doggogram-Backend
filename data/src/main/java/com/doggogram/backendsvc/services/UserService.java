6b2b265f55a4a9e553b52dc6db283a41fbc757b6
D073980 <robin.herder@sap.com>
2019-11-27 09:15:47 +0100

    Added and implemented followUser which toggles if a user follows another user

--- data/src/main/java/com/doggogram/backendsvc/services/UserService.java
+++ data/src/main/java/com/doggogram/backendsvc/services/UserService.java
@@ -1,6 +1,7 @@
 package com.doggogram.backendsvc.services;
 
 import com.doggogram.backendsvc.dto.UserDTO;
+import com.doggogram.backendsvc.util.exceptions.UserFollowException;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -10,5 +11,7 @@ public interface UserService extends RestService<List<UserDTO>> {
 
     UserDTO findUserByUser(String user);
     void registerUser(String user, String pass);
+    boolean followUser(String user, String followUser)
+    throws UserFollowException;
 
 }
--- data/src/main/java/com/doggogram/backendsvc/services/impl/UserServiceImpl.java
+++ data/src/main/java/com/doggogram/backendsvc/services/impl/UserServiceImpl.java
@@ -5,9 +5,11 @@ import com.doggogram.backendsvc.dto.UserDTO;
 import com.doggogram.backendsvc.mapper.UserMapper;
 import com.doggogram.backendsvc.repositories.UserRepository;
 import com.doggogram.backendsvc.services.UserService;
+import com.doggogram.backendsvc.util.exceptions.UserFollowException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,4 +44,27 @@ public class UserServiceImpl implements UserService {
     public List<UserDTO> getAllItems () {
         return userRepository.findAll().stream().map(userMapper::userToUserDTO).collect(Collectors.toList());
     }
+
+    @Override
+    public boolean followUser (String user, String followUser) throws EntityNotFoundException, UserFollowException {
+        User userEntity = userRepository.findUserByUser(user);
+        User followUserEntity = userRepository.findUserByUser(followUser);
+        Boolean following;
+        if(userEntity == null || followUser == null) {
+            throw new EntityNotFoundException("Cant find Entity!");
+        }
+        if(userRepository.checkIfUserFollowsUser(user, followUser) == 0) {
+            userEntity.getFollowing().add(followUserEntity);
+            following = true;
+        } else if (userRepository.checkIfUserFollowsUser(user, followUser) == 1) {
+            userEntity.getFollowing().remove(followUserEntity);
+            following = false;
+        } else {
+            userRepository.deleteRedundantFollowing(user, followUser);
+            throw new UserFollowException("Follow Data regarding user " + user + " Following " + followUser + " is corrupted. Therefore data will be dropped!");
+        }
+        userRepository.save(userEntity);
+        return following;
+    }
+
 }
