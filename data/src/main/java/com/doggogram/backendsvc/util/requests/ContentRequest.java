8fbf4217e8a71f539970220c27eacb8a8cab9a09
D073980 <robin.herder@sap.com>
2019-11-29 15:32:17 +0100

    Added

---
+++ data/src/main/java/com/doggogram/backendsvc/util/requests/ContentRequest.java
@@ -0,0 +1,12 @@
+package com.doggogram.backendsvc.util.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ContentRequest {
+
+    private String Content;
+
+}
