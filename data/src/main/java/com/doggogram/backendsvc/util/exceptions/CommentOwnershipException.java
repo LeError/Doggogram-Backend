8869e58b303a962964c1b4b61a621250449eae2e
D073980 <robin.herder@sap.com>
2019-12-05 14:45:16 +0100

    Added Exception

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/CommentOwnershipException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class CommentOwnershipException extends Exception{
+
+    public CommentOwnershipException() {
+        super();
+    }
+
+    public CommentOwnershipException(String message) {
+        super(message);
+    }
+
+}
