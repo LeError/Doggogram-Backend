0e20a0a63c815046d09758dba63f752ef8757df0
D073980 <robin.herder@sap.com>
2019-12-05 13:30:38 +0100

    Added exception

---
+++ data/src/main/java/com/doggogram/backendsvc/util/exceptions/ImageOwnershipException.java
@@ -0,0 +1,13 @@
+package com.doggogram.backendsvc.util.exceptions;
+
+public class ImageOwnershipException extends Exception {
+
+    public ImageOwnershipException() {
+        super();
+    }
+
+    public ImageOwnershipException(String message) {
+        super(message);
+    }
+
+}
