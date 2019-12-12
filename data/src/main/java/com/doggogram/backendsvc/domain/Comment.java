5a83a68c17f22410094c97cb9686dc989a1c2015
D073980 <robin.herder@sap.com>
2019-12-12 13:56:04 +0100

    Fixed Bug

--- data/src/main/java/com/doggogram/backendsvc/domain/Comment.java
+++ data/src/main/java/com/doggogram/backendsvc/domain/Comment.java
@@ -1,8 +1,9 @@
 package com.doggogram.backendsvc.domain;
 
+import com.doggogram.backendsvc.repositories.CommentRepository;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,11 +12,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PreRemove;
+import javax.persistence.Transient;
 import java.util.Date;
 
 @Data
 @Entity (name = "COMMENT")
 public class Comment {
+
+    @Transient
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -28,8 +35,14 @@ public class Comment {
     @Column (name = "COMMENT_CREATED", nullable = false)
     private Date created = new Date();
 
-    @ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "FK_USER", nullable = false)
     private User author;
+
+    @PreRemove
+    private void removeUser() {
+        author = null;
+        commentRepository.save(this);
+    }
 
 }
